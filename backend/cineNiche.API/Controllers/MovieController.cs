224e2dae800f556094bec4e2475b610a69992437
Zac Oldroyd <137462136+zoldroyd@users.noreply.github.com>
2025-04-07 15:25:27 -0600

    finiished conversion

--- backend/cineNiche.API/Controllers/MovieController.cs
+++ backend/cineNiche.API/Controllers/MovieController.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
--- frontend/src/components/WelcomeBand.tsx
+++ frontend/src/components/WelcomeBand.tsx
@@ -1,10 +1,11 @@
-function WelcomeBand(){
-    return (<>
-    <div className="row bg-primary text-white">
-        <h1 className="bold-font-header">Water Projects</h1>
-    </div>
+function WelcomeBand() {
+  return (
+    <>
+      <div className="row bg-primary text-white">
+        <h1 className="bold-font-header">CineNiche Movies</h1>
+      </div>
     </>
-    );
+  );
 }
 
 export default WelcomeBand;
