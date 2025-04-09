a9a9143e2da39b0f878061fb9f9dea172da8fd54
oliviafowler02 <130636149+oliviafowler02@users.noreply.github.com>
2025-04-08 21:26:50 -0600

    3rd auth and cookies

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,9 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import "bootstrap/dist/css/bootstrap.min.css"
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <GoogleOAuthProvider clientId="516989380594-sr6f0cdu8sj0u861eqo2dv4nbgjn4vdt.apps.googleusercontent.com">
+      <App />
+    </GoogleOAuthProvider>
   </StrictMode>,
 )
